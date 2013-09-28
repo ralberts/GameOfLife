86d27b4855f8ad22c402ae7c82a5f96b444490d8
Ryan Alberts <youthenme@hotmail.com>
2013-09-28 16:49:10 -0600

    Project Scaffolding using Yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('GameOfLifeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
